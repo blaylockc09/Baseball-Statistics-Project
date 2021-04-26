eff1737049e1edc7d8ba12d837a02093c8c7c1fa
Chris Blaylock <52610450+blaylockc09@users.noreply.github.com>
2021-04-26 16:29:37 -0400

    Delete Batter.java

--- src/baseballapp/Batter.java
+++
@@ -1,8 +0,0 @@
-
-package baseballapp;
-
-public class Batter {
-   // TODO: create the attr of the Batter class i.e. fName, lName, position..
-   // You will also need to create the getter and setter methods for this application. 
-   // 
-}
