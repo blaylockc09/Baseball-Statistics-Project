74e85e98330e21b97822776a7bbdd22c6e2aff09
Alex Crabtree <alex.crabtree6@gmail.com>
2021-04-29 15:32:46 -0400

    I added FileClass and StringUtil for the File IO and when you quit the program it writes to the file the info you put in

---
+++ src/baseballstats/StringUtil.java
@@ -0,0 +1,20 @@
+package baseballstats;
+
+/**
+ *
+ * @author alexc
+ */
+//class to help format the file to make it look nice 
+public class StringUtil {
+    public static String pad(String s, int length) {
+        if(s.length() < length){
+            StringBuilder sb = new StringBuilder(s);
+            while(sb.length() < length){
+                sb.append(" ");
+            } 
+            return sb.toString();
+        } else {
+            return s.substring(0, length);
+        }
+    }
+}
